9fff0fe4a8dc2f048aab3dace631979b6a54d22b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:18:16 +0000

    Add new files and directories and commit changes to local repository

--- language/mr/shell_extension_lang.php
+++ language/mr/shell_extension_lang.php
@@ -1,7 +1,7 @@
 <?php
 
-$lang['shell_extension_app_name'] = 'Login Shell Extension';
-$lang['shell_extension_app_description'] = 'The Login Shell Extension app provides tools to manage SSH/shell access on the system.';
-$lang['shell_extension_login_shell'] = 'Login Shell';
-$lang['shell_extension_shell'] = 'Shell';
-$lang['shell_extension_login_shell_invalid'] = 'Login shell is invalid';
+$lang['shell_extension_app_name'] = 'लॉगिन करा शेल विस्तार';
+$lang['shell_extension_app_description'] = 'लॉग-इन शेल विस्तार अनुप्रयोग प्रणालीवर एस् एस् एच् / शेल प्रवेश व्यवस्थापित करण्यासाठी साधने पुरवतो.';
+$lang['shell_extension_login_shell'] = 'लॉग इन शेल';
+$lang['shell_extension_shell'] = 'शेल';
+$lang['shell_extension_login_shell_invalid'] = 'लॉगीन शेल अवैध आहे';
