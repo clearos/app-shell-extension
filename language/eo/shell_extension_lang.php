1552dd9dd9c497475998f9347a74f70a2024851e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:12:26 +0000

    Add new files and directories and commit changes to local repository

--- language/eo/shell_extension_lang.php
+++ language/eo/shell_extension_lang.php
@@ -1,7 +1,7 @@
 <?php
 
-$lang['shell_extension_app_name'] = 'Login Shell Extension';
-$lang['shell_extension_app_description'] = 'The Login Shell Extension app provides tools to manage SSH/shell access on the system.';
-$lang['shell_extension_login_shell'] = 'Login Shell';
-$lang['shell_extension_shell'] = 'Shell';
-$lang['shell_extension_login_shell_invalid'] = 'Login shell is invalid';
+$lang['shell_extension_app_name'] = 'Ensalutu Konko Pligrandigo';
+$lang['shell_extension_app_description'] = 'La Salutnomo Konko Pligrandigo app provizas ilojn por administri SSH / ŝelo aliro al la sistemo.';
+$lang['shell_extension_login_shell'] = 'ensalutu Konko';
+$lang['shell_extension_shell'] = 'ŝelo';
+$lang['shell_extension_login_shell_invalid'] = 'Salutnomo ŝelo estas nevalida';
