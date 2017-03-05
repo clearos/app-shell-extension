5548651da74b2aeb574b6dd70ad434b7de87dded
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:09:27 +0000

    Add new files and directories and commit changes to local repository

--- language/af/shell_extension_lang.php
+++ language/af/shell_extension_lang.php
@@ -1,7 +1,7 @@
 <?php
 
-$lang['shell_extension_app_name'] = 'Login Shell Extension';
-$lang['shell_extension_app_description'] = 'The Login Shell Extension app provides tools to manage SSH/shell access on the system.';
-$lang['shell_extension_login_shell'] = 'Login Shell';
+$lang['shell_extension_app_name'] = 'Teken in Shell Uitbreiding';
+$lang['shell_extension_app_description'] = 'Die Teken Shell Uitbreiding app bied gereedskap SSH / dop toegang op die stelsel te bestuur.';
+$lang['shell_extension_login_shell'] = 'aanteken Shell';
 $lang['shell_extension_shell'] = 'Shell';
-$lang['shell_extension_login_shell_invalid'] = 'Login shell is invalid';
+$lang['shell_extension_login_shell_invalid'] = 'Teken dop is ongeldig';
