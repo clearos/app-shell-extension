c519bc04a1970c1688cc173871a99b1b0b730d0b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:17:42 +0000

    Add new files and directories and commit changes to local repository

--- language/mk/shell_extension_lang.php
+++ language/mk/shell_extension_lang.php
@@ -1,7 +1,7 @@
 <?php
 
-$lang['shell_extension_app_name'] = 'Login Shell Extension';
-$lang['shell_extension_app_description'] = 'The Login Shell Extension app provides tools to manage SSH/shell access on the system.';
-$lang['shell_extension_login_shell'] = 'Login Shell';
-$lang['shell_extension_shell'] = 'Shell';
-$lang['shell_extension_login_shell_invalid'] = 'Login shell is invalid';
+$lang['shell_extension_app_name'] = 'Влези Шел Продолжување';
+$lang['shell_extension_app_description'] = 'Во стан Најава Шел Продолжување обезбедува алатки за управување со пристап SSH / школка на системот.';
+$lang['shell_extension_login_shell'] = 'пријавете Шел';
+$lang['shell_extension_shell'] = 'школка';
+$lang['shell_extension_login_shell_invalid'] = 'Најава школка не е валиден';
