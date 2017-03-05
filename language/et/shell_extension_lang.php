5e183e7f788c23676794184b406381b9441a17f1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:12:36 +0000

    Add new files and directories and commit changes to local repository

--- language/et/shell_extension_lang.php
+++ language/et/shell_extension_lang.php
@@ -1,7 +1,7 @@
 <?php
 
-$lang['shell_extension_app_name'] = 'Login Shell Extension';
-$lang['shell_extension_app_description'] = 'The Login Shell Extension app provides tools to manage SSH/shell access on the system.';
-$lang['shell_extension_login_shell'] = 'Login Shell';
-$lang['shell_extension_shell'] = 'Shell';
-$lang['shell_extension_login_shell_invalid'] = 'Login shell is invalid';
+$lang['shell_extension_app_name'] = 'Shell Extension';
+$lang['shell_extension_app_description'] = 'Login Shell Extension app pakub vahendeid hallata SSH / shell juurdepääs süsteemile.';
+$lang['shell_extension_login_shell'] = 'Shell';
+$lang['shell_extension_shell'] = 'ümbris';
+$lang['shell_extension_login_shell_invalid'] = 'Shell on kehtetu';
