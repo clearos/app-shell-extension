11edc9c773fbecd1a8c71f1278405891cc49036d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:09:57 +0000

    Add new files and directories and commit changes to local repository

--- language/hy/shell_extension_lang.php
+++ language/hy/shell_extension_lang.php
@@ -1,7 +1,7 @@
 <?php
 
-$lang['shell_extension_app_name'] = 'Login Shell Extension';
-$lang['shell_extension_app_description'] = 'The Login Shell Extension app provides tools to manage SSH/shell access on the system.';
-$lang['shell_extension_login_shell'] = 'Login Shell';
-$lang['shell_extension_shell'] = 'Shell';
-$lang['shell_extension_login_shell_invalid'] = 'Login shell is invalid';
+$lang['shell_extension_app_name'] = 'Մուտք գործեք Shell Extension';
+$lang['shell_extension_app_description'] = 'The Մուտք Shell Extension ծրագիրը տրամադրում է գործիքներ, կառավարելու SSH / shell մուտք համակարգի վրա:';
+$lang['shell_extension_login_shell'] = 'մուտք գործել Shell';
+$lang['shell_extension_shell'] = 'վահանակ';
+$lang['shell_extension_login_shell_invalid'] = 'Մուտք Շենքերի բարակ տարածական ծածկույթների անվավեր';
