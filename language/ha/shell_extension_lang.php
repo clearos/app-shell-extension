5898004d0c435ba10e91730737b2b06665d39e5f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:14:03 +0000

    Add new files and directories and commit changes to local repository

--- language/ha/shell_extension_lang.php
+++ language/ha/shell_extension_lang.php
@@ -1,7 +1,7 @@
 <?php
 
-$lang['shell_extension_app_name'] = 'Login Shell Extension';
-$lang['shell_extension_app_description'] = 'The Login Shell Extension app provides tools to manage SSH/shell access on the system.';
-$lang['shell_extension_login_shell'] = 'Login Shell';
+$lang['shell_extension_app_name'] = 'Login Shell Tsawo';
+$lang['shell_extension_app_description'] = 'The Login Shell Tsawo app bayar da kayayyakin aiki, don sarrafa SSH / harsashi access a kan tsarin.';
+$lang['shell_extension_login_shell'] = 'login Shell';
 $lang['shell_extension_shell'] = 'Shell';
-$lang['shell_extension_login_shell_invalid'] = 'Login shell is invalid';
+$lang['shell_extension_login_shell_invalid'] = 'Login harsashi ba daidai ba ne';
