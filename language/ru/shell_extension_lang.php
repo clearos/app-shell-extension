adb294bb506cafc72e16a1f700cca92e83d76f3e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:20:17 +0000

    Add new files and directories and commit changes to local repository

--- language/ru/shell_extension_lang.php
+++ language/ru/shell_extension_lang.php
@@ -1,7 +1,7 @@
 <?php
 
-$lang['shell_extension_app_name'] = 'Login Shell Extension';
-$lang['shell_extension_app_description'] = 'The Login Shell Extension app provides tools to manage SSH/shell access on the system.';
-$lang['shell_extension_login_shell'] = 'Login Shell';
-$lang['shell_extension_shell'] = 'Shell';
-$lang['shell_extension_login_shell_invalid'] = 'Login shell is invalid';
+$lang['shell_extension_app_name'] = 'Вход Shell Extension';
+$lang['shell_extension_app_description'] = 'Приложение Войти Shell Extension предоставляет инструменты для управления доступом SSH / оболочки на систему.';
+$lang['shell_extension_login_shell'] = 'войти в систему Shell';
+$lang['shell_extension_shell'] = 'Оболочка';
+$lang['shell_extension_login_shell_invalid'] = 'Войти оболочки недействителен';
