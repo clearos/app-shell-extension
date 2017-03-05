75e767c01938069885e7b7629f903b25caa895f3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:13:41 +0000

    Add new files and directories and commit changes to local repository

--- language/gu/shell_extension_lang.php
+++ language/gu/shell_extension_lang.php
@@ -1,7 +1,7 @@
 <?php
 
-$lang['shell_extension_app_name'] = 'Login Shell Extension';
-$lang['shell_extension_app_description'] = 'The Login Shell Extension app provides tools to manage SSH/shell access on the system.';
-$lang['shell_extension_login_shell'] = 'Login Shell';
-$lang['shell_extension_shell'] = 'Shell';
-$lang['shell_extension_login_shell_invalid'] = 'Login shell is invalid';
+$lang['shell_extension_app_name'] = 'પ્રવેશ શેલ એક્સ્ટેંશન';
+$lang['shell_extension_app_description'] = 'પ્રવેશ શેલ એક્સ્ટેંશન એપ્લિકેશન સિસ્ટમ પર ssh / શેલ વપરાશ વ્યવસ્થા કરવા માટે સાધનો પૂરા પાડે છે.';
+$lang['shell_extension_login_shell'] = 'પ્રવેશ શેલ';
+$lang['shell_extension_shell'] = 'શેલ';
+$lang['shell_extension_login_shell_invalid'] = 'પ્રવેશ શેલ અમાન્ય છે';
