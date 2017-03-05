2ba1921012586b38fda524f845f40f82a04a3971
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:10:30 +0000

    Add new files and directories and commit changes to local repository

--- language/be/shell_extension_lang.php
+++ language/be/shell_extension_lang.php
@@ -1,7 +1,7 @@
 <?php
 
-$lang['shell_extension_app_name'] = 'Login Shell Extension';
-$lang['shell_extension_app_description'] = 'The Login Shell Extension app provides tools to manage SSH/shell access on the system.';
-$lang['shell_extension_login_shell'] = 'Login Shell';
-$lang['shell_extension_shell'] = 'Shell';
-$lang['shell_extension_login_shell_invalid'] = 'Login shell is invalid';
+$lang['shell_extension_app_name'] = 'Уваход Shell Extension';
+$lang['shell_extension_app_description'] = 'Дадатак Увайсці Shell Extension падае прылады для кіравання доступам SSH / абалонкі на сістэму.';
+$lang['shell_extension_login_shell'] = 'увайсці ў сістэму Shell';
+$lang['shell_extension_shell'] = 'абалонка';
+$lang['shell_extension_login_shell_invalid'] = 'Увайсці абалонкі ня дзейнічае';
