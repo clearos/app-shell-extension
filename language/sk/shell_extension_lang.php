8a378bf6ca354509adc05fef7c316697452472f2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:20:51 +0000

    Add new files and directories and commit changes to local repository

--- language/sk/shell_extension_lang.php
+++ language/sk/shell_extension_lang.php
@@ -1,7 +1,7 @@
 <?php
 
-$lang['shell_extension_app_name'] = 'Login Shell Extension';
-$lang['shell_extension_app_description'] = 'The Login Shell Extension app provides tools to manage SSH/shell access on the system.';
-$lang['shell_extension_login_shell'] = 'Login Shell';
-$lang['shell_extension_shell'] = 'Shell';
-$lang['shell_extension_login_shell_invalid'] = 'Login shell is invalid';
+$lang['shell_extension_app_name'] = 'Prihlásenie Shell Extension';
+$lang['shell_extension_app_description'] = 'Aplikácia Vstup Shell Extension poskytuje nástroje pre správu prístupu SSH / shell v systéme.';
+$lang['shell_extension_login_shell'] = 'prihlasovacie shell';
+$lang['shell_extension_shell'] = 'škrupina';
+$lang['shell_extension_login_shell_invalid'] = 'Login shell je neplatná';
