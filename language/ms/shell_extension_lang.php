b766f539475b4bd90b2015443e5012e220604138
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:18:27 +0000

    Add new files and directories and commit changes to local repository

--- language/ms/shell_extension_lang.php
+++ language/ms/shell_extension_lang.php
@@ -1,7 +1,7 @@
 <?php
 
-$lang['shell_extension_app_name'] = 'Login Shell Extension';
-$lang['shell_extension_app_description'] = 'The Login Shell Extension app provides tools to manage SSH/shell access on the system.';
-$lang['shell_extension_login_shell'] = 'Login Shell';
+$lang['shell_extension_app_name'] = 'Log masuk Sambungan Kerangka';
+$lang['shell_extension_app_description'] = 'Aplikasi Daftar Sambungan Kerangka menyediakan alat untuk menguruskan akses SSH / shell pada sistem.';
+$lang['shell_extension_login_shell'] = 'melog masuk Shell';
 $lang['shell_extension_shell'] = 'Shell';
-$lang['shell_extension_login_shell_invalid'] = 'Login shell is invalid';
+$lang['shell_extension_login_shell_invalid'] = 'shell log masuk tidak sah';
