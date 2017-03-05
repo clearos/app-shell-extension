24a2963e37bd829f917dfa93bec823aec58bb71a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:24:37 +0000

    Add new files and directories and commit changes to local repository

--- language/zu/shell_extension_lang.php
+++ language/zu/shell_extension_lang.php
@@ -1,7 +1,7 @@
 <?php
 
-$lang['shell_extension_app_name'] = 'Login Shell Extension';
-$lang['shell_extension_app_description'] = 'The Login Shell Extension app provides tools to manage SSH/shell access on the system.';
-$lang['shell_extension_login_shell'] = 'Login Shell';
-$lang['shell_extension_shell'] = 'Shell';
-$lang['shell_extension_login_shell_invalid'] = 'Login shell is invalid';
+$lang['shell_extension_app_name'] = 'Ngena ngemvume Shell Extension';
+$lang['shell_extension_app_description'] = 'Insiza Login Igobolondo Extension inikeza amathuluzi ukuphatha ssh / igobolondo ukufinyelela ohlelweni.';
+$lang['shell_extension_login_shell'] = 'ngemvume Shell';
+$lang['shell_extension_shell'] = 'shell';
+$lang['shell_extension_login_shell_invalid'] = 'shell Login ayivumelekile';
