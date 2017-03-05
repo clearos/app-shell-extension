4c52e4bdaa060eb0b2ed04d2d5dc55b2ce6db102
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:21:35 +0000

    Add new files and directories and commit changes to local repository

--- language/es/shell_extension_lang.php
+++ language/es/shell_extension_lang.php
@@ -1,7 +1,7 @@
 <?php
 
-$lang['shell_extension_app_name'] = 'Login Shell Extension';
-$lang['shell_extension_app_description'] = 'The Login Shell Extension app provides tools to manage SSH/shell access on the system.';
-$lang['shell_extension_login_shell'] = 'Login Shell';
-$lang['shell_extension_shell'] = 'Shell';
-$lang['shell_extension_login_shell_invalid'] = 'Login shell is invalid';
+$lang['shell_extension_app_name'] = 'Accede Shell Extension';
+$lang['shell_extension_app_description'] = 'La aplicación de sesión Shell Extension proporciona herramientas para gestionar el acceso SSH / shell en el sistema.';
+$lang['shell_extension_login_shell'] = 'iniciar sesión Shell';
+$lang['shell_extension_shell'] = 'Cáscara';
+$lang['shell_extension_login_shell_invalid'] = 'Shell de entrada no es válida';
