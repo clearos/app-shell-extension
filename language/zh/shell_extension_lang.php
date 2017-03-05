4b5bee5016cd93bf4e21dfa030bc375aa75dbfea
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:11:43 +0000

    Add new files and directories and commit changes to local repository

--- language/zh/shell_extension_lang.php
+++ language/zh/shell_extension_lang.php
@@ -1,7 +1,7 @@
 <?php
 
-$lang['shell_extension_app_name'] = 'Login Shell Extension';
-$lang['shell_extension_app_description'] = 'The Login Shell Extension app provides tools to manage SSH/shell access on the system.';
-$lang['shell_extension_login_shell'] = 'Login Shell';
-$lang['shell_extension_shell'] = 'Shell';
-$lang['shell_extension_login_shell_invalid'] = 'Login shell is invalid';
+$lang['shell_extension_app_name'] = '登录shell扩展';
+$lang['shell_extension_app_description'] = '登录shell扩展的应用程序提供了工具来管理系统上的SSH / shell访问。';
+$lang['shell_extension_login_shell'] = '登录shell';
+$lang['shell_extension_shell'] = '贝壳';
+$lang['shell_extension_login_shell_invalid'] = '登录shell是无效的';
