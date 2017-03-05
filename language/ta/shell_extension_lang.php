afdd6b2a1ae2832204595280a33d757b314f0de6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:22:20 +0000

    Add new files and directories and commit changes to local repository

--- language/ta/shell_extension_lang.php
+++ language/ta/shell_extension_lang.php
@@ -1,7 +1,7 @@
 <?php
 
-$lang['shell_extension_app_name'] = 'Login Shell Extension';
-$lang['shell_extension_app_description'] = 'The Login Shell Extension app provides tools to manage SSH/shell access on the system.';
-$lang['shell_extension_login_shell'] = 'Login Shell';
-$lang['shell_extension_shell'] = 'Shell';
-$lang['shell_extension_login_shell_invalid'] = 'Login shell is invalid';
+$lang['shell_extension_app_name'] = 'ஷெல் நீட்டிப்பு உள்நுழைய';
+$lang['shell_extension_app_description'] = 'தேதி ஷெல் நீட்டிப்பு பயன்பாட்டை கணினியில் ssh / ஷெல்லை நிர்வகிக்க கருவிகள் வழங்குகிறது.';
+$lang['shell_extension_login_shell'] = 'உள்நுழைய ஷெல்';
+$lang['shell_extension_shell'] = 'ஓடு';
+$lang['shell_extension_login_shell_invalid'] = 'தேதி ஷெல் தவறானது';
