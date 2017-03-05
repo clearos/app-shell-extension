76460a4e75e4536495a4581528e766c7d614f66a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:09:47 +0000

    Add new files and directories and commit changes to local repository

--- language/ar/shell_extension_lang.php
+++ language/ar/shell_extension_lang.php
@@ -1,7 +1,7 @@
 <?php
 
-$lang['shell_extension_app_name'] = 'Login Shell Extension';
-$lang['shell_extension_app_description'] = 'The Login Shell Extension app provides tools to manage SSH/shell access on the system.';
-$lang['shell_extension_login_shell'] = 'Login Shell';
-$lang['shell_extension_shell'] = 'Shell';
-$lang['shell_extension_login_shell_invalid'] = 'Login shell is invalid';
+$lang['shell_extension_app_name'] = 'تسجيل الدخول شل التمديد';
+$lang['shell_extension_app_description'] = 'ويوفر التطبيق الدخول شل التمديد أدوات لإدارة وصول SSH / قذيفة على النظام.';
+$lang['shell_extension_login_shell'] = 'تسجيل الدخول شل';
+$lang['shell_extension_shell'] = 'الصدف';
+$lang['shell_extension_login_shell_invalid'] = 'الدخول قذيفة غير صالحة';
