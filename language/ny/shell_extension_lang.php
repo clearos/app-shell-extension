3d494fbb6af70974dfae28f7eb4ff463a3ee32a6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:11:32 +0000

    Add new files and directories and commit changes to local repository

--- language/ny/shell_extension_lang.php
+++ language/ny/shell_extension_lang.php
@@ -1,7 +1,7 @@
 <?php
 
-$lang['shell_extension_app_name'] = 'Login Shell Extension';
-$lang['shell_extension_app_description'] = 'The Login Shell Extension app provides tools to manage SSH/shell access on the system.';
-$lang['shell_extension_login_shell'] = 'Login Shell';
-$lang['shell_extension_shell'] = 'Shell';
-$lang['shell_extension_login_shell_invalid'] = 'Login shell is invalid';
+$lang['shell_extension_app_name'] = 'Kulowa Nkhono Extension';
+$lang['shell_extension_app_description'] = 'The app Malowedwe Nkhono Extension mabuku okuthandizani kugwiritsa ntchito mwayi SSH / chipolopolo pa dongosolo.';
+$lang['shell_extension_login_shell'] = 'kulowa Nkhono';
+$lang['shell_extension_shell'] = 'chipolopolo';
+$lang['shell_extension_login_shell_invalid'] = 'Malowedwe chipolopolo silololedwa';
