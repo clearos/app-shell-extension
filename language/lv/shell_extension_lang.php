57483fad13e0f7e6f4b957e066f0370e8e709d7a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-05 01:17:09 +0000

    Add new files and directories and commit changes to local repository

--- language/lv/shell_extension_lang.php
+++ language/lv/shell_extension_lang.php
@@ -1,7 +1,7 @@
 <?php
 
-$lang['shell_extension_app_name'] = 'Login Shell Extension';
-$lang['shell_extension_app_description'] = 'The Login Shell Extension app provides tools to manage SSH/shell access on the system.';
-$lang['shell_extension_login_shell'] = 'Login Shell';
-$lang['shell_extension_shell'] = 'Shell';
-$lang['shell_extension_login_shell_invalid'] = 'Login shell is invalid';
+$lang['shell_extension_app_name'] = 'Pieteikties Shell Extension';
+$lang['shell_extension_app_description'] = 'Pieteikšanās Shell Extension app nodrošina instrumentus, lai pārvaldītu SSH / shell piekļuvi sistēmai.';
+$lang['shell_extension_login_shell'] = 'pieteikties Shell';
+$lang['shell_extension_shell'] = 'apvalks';
+$lang['shell_extension_login_shell_invalid'] = 'Login apvalks ir nederīgs';
